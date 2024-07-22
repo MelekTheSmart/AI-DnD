5edf6658f7971fa9a3385b8ac6c0e013e9d74670
merri <merrickshurtliff@gmail.com>
2024-07-22 13:02:42 -0600

    End of day 1 of last week

--- client/map/Map.js
+++ client/map/Map.js
@@ -1,36 +1,199 @@
-
 function initApp() {
   // Initialize PIXI.js application
 
   let gameWindow = document.getElementById("game-container");
   let html = document.documentElement;
 
-  var state = GameState(gameWindow, html, [])
+  var state = GameState(gameWindow, html, []);
 
   let app = new PIXI.Application({
     width: state.getWidth(),
     height: state.getHeight(),
     backgroundColor: 0x1099bb,
   });
-  
+
   // I would like to initialize all the variables 'state' needs in one function rather than spreading them out here.
 
-  state.app = app
+  state.app = app;
 
   state.window.appendChild(state.app.view); // app.view is a canvas element
 
-  state.tileSize = 64;
+  state.tileSize = 128;
 
   state.isResizing = false;
 
   state.isPanning = false;
 
-  // Create a container for the map
+  state.dragTarget = null;
 
-  dragAndDropInit(state.window);
+  state.rotateTarget = null;
+
+  dragAndDropInit(state.window, state);
   return state;
 }
+function createSprite(texture, x, y, state) {
+  const sprite = new PIXI.Sprite(texture);
+  // sprite.position.set(x, y);
+  sprite.anchor.set(0.5);
+  sprite.interactiveChildren = true;
+  let isDragging = false;
+  let dragData = null;
 
+  const outline = new PIXI.Graphics();
+  outline.lineStyle(2, 0x00ffff); // Green border, 2px thick
+  outline.drawRect(
+    -sprite.width / 2,
+    -sprite.height / 2,
+    sprite.width,
+    sprite.height
+  ); // x, y, width, height\
+  // outline.visible = false;
+  outline.width = sprite.width;
+  outline.height = sprite.height;
+  const rotateHandle = new PIXI.Graphics();
+  rotateHandle.interactive = true;
+  rotateHandle.buttonMode = true;
+  rotateHandle.lineStyle(2, 0x00ffff);
+  rotateHandle.beginFill(0x00ffff);
+  rotateHandle.drawCircle(0, -outline.height / 2 - 10, 10, 10);
+  rotateHandle.endFill();
+  rotateHandle.on("mousedown", (event) => {
+    state.rotateTarget = sprite;
+    lastSizeX = event.data.global.x;
+    lastSizeY = event.data.global.y;
+    mouseOffsetX = event.global.x;
+    mouseOffsetY = event.global.y;
+  });
+  let vars = {
+    0: {
+      x: -outline.width / 2 - 10,
+      y: -outline.height / 2 - 10,
+      directionx: -1,
+      directiony: -1,
+    },
+    1: {
+      x: outline.width / 2,
+      y: -outline.height / 2 - 10,
+      directionx: 1,
+      directiony: -1,
+    },
+    2: {
+      x: -outline.width / 2 - 10,
+      y: outline.height / 2,
+      directionx: -1,
+      directiony: 1,
+    },
+    3: {
+      x: outline.width / 2,
+      y: outline.height / 2,
+      directionx: 1,
+      directiony: 1,
+    },
+  };
+
+  for (let i = 0; i < 4; i++) {
+    const resizeHandle = new PIXI.Graphics();
+    resizeHandle.interactive = true;
+    resizeHandle.buttonMode = true;
+    resizeHandle.lineStyle(2, 0x00ffff);
+    resizeHandle.beginFill(0x00ffff);
+
+    let { x, y, directionx, directiony } = vars[i];
+
+    resizeHandle.drawRect(x, y, 10, 10);
+    resizeHandle.on("mousedown", () => {
+      state.directionx = directionx;
+      state.directiony = directiony;
+    });
+
+    resizeHandle.endFill();
+    resizeHandle.addEventListener("mousedown", (event) => {
+      resizeTarget = sprite;
+      lastSizeX = event.data.global.x;
+      lastSizeY = event.data.global.y;
+      state.isResizing = true;
+      mouseOffsetX = event.global.x;
+      mouseOffsetY = event.global.y;
+    });
+    resizeHandle.on("mouseup", (event) => {
+      state.isResizing = false;
+      console.log("disabled");
+    });
+    resizeHandle.on("mouseupoutside", (event) => {
+      state.isResizing = false;
+    });
+
+    outline.addChild(resizeHandle);
+  }
+  outline.addChild(rotateHandle);
+  // Rectangle with fill and border
+  const spriteCont = new PIXI.Container();
+  const core = new PIXI.Container();
+  core.interactive = true;
+  spriteCont.interactiveChildren = true;
+  spriteCont.x = x;
+  spriteCont.y = y;
+  console.log(spriteCont.width);
+  const outline_rect = new PIXI.Graphics();
+  outline_rect.lineStyle(2, 0x00ff00); // Green border, 2px thick
+  outline_rect.drawRect(0, 0, spriteCont.width, spriteCont.height); // x, y, width, height
+  spriteCont.addChild(outline_rect);
+  const node = new PIXI.Graphics();
+  node.lineStyle(2, 0x00ff00); // Green border, 2px thick
+  node.drawCircle(0, 0, state.tileSize / 2, state.tileSize / 2); // x, y, width, height
+  node.hitArea = new PIXI.Circle(0, 0, state.tileSize / 2, state.tileSize / 2);
+  node.visible = true;
+  node.interactive = true;
+  node.buttonMode = true;
+  node.on("mousedown", (event) => {
+    if (true) {
+      state.dragTarget = spriteCont;
+      actualX = spriteCont.x;
+      actualY = spriteCont.y;
+      isDragging = true;
+      console.log(state.mapContainer.children);
+      state.mapContainer.setChildIndex(
+        spriteCont,
+        state.mapContainer.children.length - 1
+      );
+      spriteCont.alpha = 0.5;
+      dragData = event.data;
+      state.lastX = event.data.global.x;
+      state.lastY = event.data.global.y;
+    }
+  });
+  spriteCont.addChild(sprite);
+  spriteCont.addChild(node);
+  spriteCont.addChild(core);
+  sprite.addChild(outline);
+  core.on("globalpointermove", (event) => {
+    if (spriteCont.getBounds().contains(event.global.x, event.global.y)) {
+      node.visible = true;
+    } else {
+      node.visible = false;
+    }
+  });
+
+  spriteCont.on("pointerup", () => {
+    if (isDragging) {
+      spriteCont.x = state.tieSize * Math.round(spriteCont.x / state.tieSize);
+      spriteCont.y = state.tieSize * Math.round(spriteCont.y / state.tieSize);
+      spriteCont.alpha = 1;
+      isDragging = false;
+      dragData = null;
+    }
+  });
+
+  sprite.on("pointerupoutside", () => {
+    isDragging = false;
+    dragData = null;
+  });
+  sprite.on("wheel", (event) => {
+    console.log(event);
+  });
+  // spriteCont.hitArea = new PIXI.Circle(0, 0, 32, 32);
+  state.mapContainer.addChild(spriteCont);
+}
 async function initMapContainer(state) {
   let mapContainer = new PIXI.Container();
   state.app.stage.addChild(mapContainer);
@@ -41,15 +204,16 @@ async function initMapContainer(state) {
 
   gridContainer.tiles = [];
 
-
-  gridContainer.createTile = function(x, y) {
+  gridContainer.createTile = function (x, y) {
     const tile = new PIXI.Sprite(this.tileTexture);
     tile.anchor.set(0.5);
     tile.position.set(x, y);
+    tile.width = state.tileSize;
+    tile.height = state.tileSize;
     this.addChild(tile);
     this.setChildIndex(tile, 0);
     return tile;
-  }
+  };
 
   mapContainer.addChild(gridContainer);
   mapContainer.interactive = true;
@@ -81,20 +245,60 @@ async function initMapContainer(state) {
       const deltaSX = event.data.global.x - lastSizeX;
       const deltaSY = event.data.global.y - lastSizeY;
       if (Math.abs(deltaSX) > Math.abs(deltaSY)) {
-        resizeTarget.width = resizeTarget.width + deltaSX * 1.2 * state.directionx;
-        resizeTarget.height = resizeTarget.height + deltaSX * 1.2 * state.directionx;
-        }
-      else if (Math.abs(deltaSX) < Math.abs(deltaSY)) {
-        resizeTarget.width = resizeTarget.width + deltaSY * 1.2 * state.directiony;
-        resizeTarget.height = resizeTarget.height + deltaSY * 1.2 * state.directiony;
+        resizeTarget.width =
+          resizeTarget.width + deltaSX * 1.2 * state.directionx;
+        resizeTarget.height =
+          resizeTarget.height + deltaSX * 1.2 * state.directionx;
+      } else if (Math.abs(deltaSX) < Math.abs(deltaSY)) {
+        resizeTarget.width =
+          resizeTarget.width + deltaSY * 1.2 * state.directiony;
+        resizeTarget.height =
+          resizeTarget.height + deltaSY * 1.2 * state.directiony;
       }
       lastSizeX = event.data.global.x;
       lastSizeY = event.data.global.y;
+    }
+    if (state.dragTarget) {
+      const deltaSX = event.data.global.x - state.lastX;
+      const deltaSY = event.data.global.y - state.lastY;
+      state.dragTarget.x = state.dragTarget.x + deltaSX;
+      state.dragTarget.y = state.dragTarget.y + deltaSY;
+      actualX = actualX + deltaSX;
+      actualY = actualY + deltaSY;
+      state.dragTarget.x =
+        state.tileSize * Math.round(actualX / state.tileSize);
+      state.dragTarget.y =
+        state.tileSize * Math.round(actualY / state.tileSize);
+      state.lastX = event.data.global.x;
+      state.lastY = event.data.global.y;
+    }
+    if (state.rotateTarget) {
+      state.rotateTarget.rotation =
+        Math.atan2(
+          event.global.y - state.rotateTarget.getGlobalPosition().y,
+          event.global.x - state.rotateTarget.getGlobalPosition().x
+        ) +
+        Math.PI / 2;
     }
   });
 
   mapContainer.on("pointerup", () => {
     mapContainer.isDragging = false;
+    isMapDragging = false;
+    if (state.dragTarget) {
+      state.dragTarget.x =
+        state.tileSize * Math.round(state.dragTarget.x / state.tileSize);
+      state.dragTarget.y =
+        state.tileSize * Math.round(state.dragTarget.y / state.tileSize);
+      didMove = false;
+      state.dragTarget.alpha = 1;
+      state.isDragging = false;
+      state.dragTarget = null;
+      dragData = null;
+    }
+    if (state.rotateTarget) {
+      state.rotateTarget = null;
+    }
   });
 
   mapContainer.on("pointerupoutside", () => {
@@ -104,7 +308,7 @@ async function initMapContainer(state) {
   state.mapContainer = mapContainer;
 }
 
-function dragAndDropInit(element) {
+function dragAndDropInit(element, state) {
   function dropHandler(event) {
     event.preventDefault();
     console.log("File(s) dropped");
@@ -119,8 +323,9 @@ function dragAndDropInit(element) {
       console.log(
         createSprite(
           dragTexture,
-          event.x - mapContainer.x,
-          event.y - mapContainer.y
+          event.x - state.mapContainer.x,
+          event.y - state.mapContainer.y,
+          state
         )
       );
     };
@@ -140,7 +345,6 @@ function dragAndDropInit(element) {
 }
 
 async function main() {
-
   var state = initApp();
   await initMapContainer(state);
   console.log(state.mapContainer);
@@ -169,187 +373,12 @@ async function main() {
     false
   );
 
-  function createSprite(texture, x, y) {
-    const sprite = new PIXI.Sprite(texture);
-    // sprite.position.set(x, y);
-    sprite.anchor.set(0.5);
-
-    let isDragging = false;
-    let dragData = null;
-
-    const outline = new PIXI.Graphics();
-    outline.lineStyle(2, 0x00ffff); // Green border, 2px thick
-    outline.drawRect(
-      -sprite.width / 2,
-      -sprite.height / 2,
-      sprite.width,
-      sprite.height
-    ); // x, y, width, height\
-    // outline.visible = false;
-    outline.width = sprite.width;
-    outline.height = sprite.height;
-    const rotateHandle = new PIXI.Graphics();
-    rotateHandle.interactive = true;
-    rotateHandle.buttonMode = true;
-    rotateHandle.lineStyle(2, 0x00ffff);
-    rotateHandle.beginFill(0x00ffff);
-    rotateHandle.drawCircle(
-      -outline.width / 4 + 100,
-      -outline.height / 2 - 10,
-      10,
-      10
-    );
-    rotateHandle.endFill();
-    let vars = {
-      0: {
-        x: -outline.width / 2 - 10,
-        y: -outline.height /2 - 10,
-        directionx: -1,
-        directiony: -1
-      },
-      1: {
-        x: outline.width / 2,
-        y: -outline.height / 2 - 10,
-        directionx: 1,
-        directiony: -1
-      },
-      2: {
-        x: -outline.width / 2 - 10,
-        y: outline.height /2,
-        directionx: -1,
-        directiony: 1
-      },
-      3: {
-        x: outline.width / 2,
-        y: outline.height /2,
-        directionx: 1,
-        directiony: 1
-      }
-    }
-
-    for (let i = 0; i < 4; i++) {
-      const resizeHandle = new PIXI.Graphics();
-      resizeHandle.interactive = true;
-      resizeHandle.buttonMode = true;
-      resizeHandle.lineStyle(2, 0x00ffff);
-      resizeHandle.beginFill(0x00ffff);
-      
-      let { x, y, directionx, directiony } = vars[i];
-
-      resizeHandle.drawRect(
-        x,
-        y,
-        10,
-        10
-      );
-      resizeHandle.on("mousedown", () => {
-        state.directionx = directionx;
-        state.directiony = directiony;
-      });
-
-      resizeHandle.endFill();
-      resizeHandle.on("mousedown", (event) => {
-        resizeTarget = sprite;
-        lastSizeX = event.data.global.x;
-        lastSizeY = event.data.global.y;
-        state.isResizing = true;
-        mouseOffsetX = event.global.x;
-        mouseOffsetY = event.global.y;
-      });
-      resizeHandle.on("mouseup", (event) => {
-        state.isResizing = false;
-        console.log("disabled");
-      });
-      resizeHandle.on("mouseupoutside", (event) => {
-        state.isResizing = false;
-      });
-
-      outline.addChild(resizeHandle);
-    }
-    outline.addChild(rotateHandle);
-    // Rectangle with fill and border
-    const spriteCont = new PIXI.Container();
-    spriteCont.interactive = true;
-    spriteCont.x = x;
-    spriteCont.y = y;
-    console.log(spriteCont.width);
-    const outline_rect = new PIXI.Graphics();
-    outline_rect.lineStyle(2, 0x00ff00); // Green border, 2px thick
-    outline_rect.drawRect(0, 0, spriteCont.width, spriteCont.height); // x, y, width, height
-    spriteCont.addChild(outline_rect);
-    const node = new PIXI.Graphics();
-    node.lineStyle(2, 0x00ff00); // Green border, 2px thick
-    node.drawCircle(0, 0, 32, 32); // x, y, width, height
-    node.hitArea = new PIXI.Circle(0, 0, 32, 32);
-    node.visible = true;
-    node.interactive = true;
-    node.buttonMode = true;
-    node.on("mousedown", (event) => {
-      actualX = spriteCont.x;
-      actualY = spriteCont.y;
-      isDragging = true;
-      state.mapContainer.setChildIndex(spriteCont, state.mapContainer.children.length - 1);
-      spriteCont.alpha = 0.5;
-      // isMapDragging = false;
-      dragData = event.data;
-      lastX = event.data.global.x;
-      lastY = event.data.global.y;
-    });
-    spriteCont.addChild(sprite);
-    spriteCont.addChild(node);
-    sprite.addChild(outline);
-
-    spriteCont.on("rightclick", (event) => {
-      console.log(spriteCont);
-    });
-    spriteCont.on("pointerover", (event) => {
-      node.visible = true;
-    });
-    spriteCont.on("pointerout", (event) => {
-      node.visible = false;
-    });
-
-    spriteCont.on("pointermove", (event) => {
-      if (isDragging) {
-        const deltaSX = event.data.global.x - lastX;
-        const deltaSY = event.data.global.y - lastY;
-        spriteCont.x = spriteCont.x + deltaSX;
-        spriteCont.y = spriteCont.y + deltaSY;
-        actualX = actualX + deltaSX;
-        actualY = actualY + deltaSY;
-        spriteCont.x = 64 * Math.round(actualX / 64);
-        spriteCont.y = 64 * Math.round(actualY / 64);
-        lastX = event.data.global.x;
-        lastY = event.data.global.y;
-      }
-    });
-
-    spriteCont.on("pointerup", () => {
-      if (isDragging) {
-        spriteCont.x = 64 * Math.round(spriteCont.x / 64);
-        spriteCont.y = 64 * Math.round(spriteCont.y / 64);
-        spriteCont.alpha = 1;
-        isDragging = false;
-        dragData = null;
-      }
-    });
-
-    sprite.on("pointerupoutside", () => {
-      isDragging = false;
-      dragData = null;
-    });
-    sprite.on("wheel", (event) => {
-      console.log(event);
-    });
-    state.mapContainer.addChild(spriteCont);
-  }
-
   const spriteTexture = await PIXI.Assets.load("sample.png");
   // const asset = PIXI.Loader.load("sample.png");
   // console.log(asset);
-  createSprite(spriteTexture, 200, 200);
-  createSprite(spriteTexture, 300, 300);
-  createSprite(spriteTexture, 700, 700);
+  createSprite(spriteTexture, 200, 200, state);
+  createSprite(spriteTexture, 300, 300, state);
+  createSprite(spriteTexture, 700, 700, state);
 
   // Function to create a tile
 
@@ -363,8 +392,8 @@ async function main() {
     const gridX = Math.floor(cameraX / state.tileSize);
     const gridY = Math.floor(cameraY / state.tileSize) + 1;
 
-    gridContainer.height = state.getHeight();
-    gridContainer.width = state.getWidth();
+    // gridContainer.height = state.getHeight();
+    // gridContainer.width = state.getWidth();
 
     gridContainer.children.forEach((tile) => {
       let tileX = tile.position.x;
@@ -411,7 +440,6 @@ async function main() {
   window.addEventListener("resize", () => {
     // state.app.renderer.resize(window.innerWidth, window.innerHeight);
   });
-};
+}
 
 main();
-
