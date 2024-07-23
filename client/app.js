0831416bc3aca238385d9c6f9d97fc383408dae6
merri <merrickshurtliff@gmail.com>
2024-07-23 12:30:11 -0600

    day 2 of final week, pushing frontend

--- client/app.js
+++ client/app.js
@@ -5,9 +5,23 @@ Vue.createApp({
   data() {
     return {
       titleh1: "Template",
+      users: {},
+      sidebarTab: "creatures",
+      sidebarShow: true,
+      chatInputField: "",
+      messageHistory: ["Hello"],
     };
   },
-  methods: {},
+  methods: {
+    setTab: function (tabId) {
+      this.sidebarTab = tabId;
+    },
+    addMessage: function () {
+      console.log("hello");
+      this.messageHistory.push(this.chatInputField);
+      this.chatInputField = "";
+    },
+  },
   created: function () {
     console.log("vue app loaded!");
   },
