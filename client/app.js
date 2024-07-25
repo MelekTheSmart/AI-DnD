c17f59ee76906906a789f1ff879cde2a758cc693
merri <merrickshurtliff@gmail.com>
2024-07-25 11:05:42 -0600

    added stuff

--- client/app.js
+++ client/app.js
@@ -6,10 +6,57 @@ Vue.createApp({
     return {
       titleh1: "Template",
       users: {},
-      sidebarTab: "creatures",
+      sidebarTab: "showGeneratedStatblock",
       sidebarShow: true,
       chatInputField: "",
       messageHistory: ["Hello"],
+      currentCreature: {
+        name: "Goblin",
+        size: "Small",
+        type: "Humanoid",
+        alignment: "Neutral Evil",
+        armorClass: 15,
+        hitPoints: 7,
+        speed: {
+          walk: 30,
+        },
+        abilityScores: {
+          strength: 8,
+          dexterity: 14,
+          constitution: 10,
+          intelligence: 10,
+          wisdom: 8,
+          charisma: 8,
+        },
+        savingThrows: {},
+        skills: [
+          {
+            name: "Stealth",
+            modifier: 6,
+          },
+        ],
+        senses: {"Darkvision", " 60 ft."},
+        languages: ["Common", "Goblin"],
+        challengeRating: 0.25,
+        specialAbilities: [
+          {
+            name: "Nimble Escape",
+            description:
+              "The goblin can take the Disengage or Hide action as a bonus action on each of its turns.",
+          },
+        ],
+        actions: [
+          {
+            name: "Scimitar",
+            description:
+              "Melee Weapon Attack: +4 to hit, reach 5 ft., one target. Hit: 5 (1d6 + 2) slashing damage.",
+            attackBonus: 4,
+            damage: "1d6+2",
+          },
+        ],
+        legendaryActions: [],
+        homebrew: [],
+      },
     };
   },
   methods: {
@@ -19,6 +66,40 @@ Vue.createApp({
     addMessage: function () {
       this.messageHistory.unshift(this.chatInputField);
       this.chatInputField = "";
+    },
+    checkObjeck: function (object) {
+      if (!(typeof object == "object")) {
+        return object;
+      }
+      for (let item in object) {
+        if (typeof object[item] == "object") {
+          for (let key in object[item]) {
+            console.log(key, object[item][key]);
+          }
+        }
+        return item;
+      }
+    },
+    getKeysAndValues: function (obj) {
+      let keysAndValues = [];
+
+      for (let key in obj) {
+        if (obj.hasOwnProperty(key)) {
+          if (typeof obj[key] === "object" && obj[key] !== null) {
+            let nested = this.getKeysAndValues(obj[key]);
+            keysAndValues.push(key);
+            // Push each key-value pair from nested object\
+            nested.forEach((entry) => {
+              keysAndValues.push(`${entry}`);
+            });
+          } else {
+            // Push key-value pair for primitive values
+            keysAndValues.push(`${key}:${obj[key]}`);
+          }
+        }
+      }
+
+      return keysAndValues;
     },
     async sendRequest() {
       // Add user message to history
@@ -62,5 +143,10 @@ Vue.createApp({
   },
   created: function () {
     console.log("vue app loaded!");
+  },
+  computed: {
+    getCreatureList: function () {
+      console.log(this.getKeysAndValues(this.currentCreature));
+    },
   },
 }).mount("#app");
