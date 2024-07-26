f20a51e1080ed1a2c8c164fc2f323545343a66c0
merri <merrickshurtliff@gmail.com>
2024-07-25 18:40:15 -0600

    made progress with frontend

--- client/app.js
+++ client/app.js
@@ -4,12 +4,47 @@ const URL = "http://localhost:8080";
 Vue.createApp({
   data() {
     return {
+      user: {},
       titleh1: "Template",
       users: {},
       sidebarTab: "showGeneratedStatblock",
       sidebarShow: true,
       chatInputField: "",
       messageHistory: ["Hello"],
+      colors: [
+        "FCE205",
+        "FCB201",
+        "ED6A09",
+        "F9965B",
+        "A51002",
+        "DB1102",
+        "CD5D51",
+        "FA187D",
+        "F34C74",
+        "FB99A6",
+        "FC4648",
+        "A7034C",
+        "81539C",
+        "BD93D3",
+        "92BAE5",
+        "B8E4FD",
+        "0479CB",
+        "050573",
+        "028C7D",
+        "41E2A3",
+        "495A10",
+        "6CA138",
+        "01A210",
+        "84E248",
+        "F1D18B",
+        "9F7A46",
+        "773E0B",
+        "33200C",
+        "FFFEFC",
+        "ACACAC",
+        "121212",
+      ],
+
       currentCreature: {
         name: "Goblin",
         size: "Small",
@@ -19,6 +54,10 @@ Vue.createApp({
         hitPoints: 7,
         speed: {
           walk: 30,
+          fly: 0,
+          swim: 0,
+          climb: 0,
+          burrow: 0,
         },
         abilityScores: {
           strength: 8,
@@ -28,16 +67,21 @@ Vue.createApp({
           wisdom: 8,
           charisma: 8,
         },
-        savingThrows: {},
+        savingThrows: {
+          strength: -1,
+          dexterity: +2,
+          constitution: +0,
+          intelligence: +0,
+          wisdom: -1,
+          charisma: -1,
+        },
         skills: [
-          {
-            name: "Stealth",
-            modifier: 6,
-          },
+          { name: "Stealth", modifier: +6 },
+          { name: "Deception", modifier: +1 },
         ],
-        senses: {"Darkvision", " 60 ft."},
+        senses: ["Darkvision 60 ft.", "blindsight 10 ft."],
         languages: ["Common", "Goblin"],
-        challengeRating: 0.25,
+        challengeRating: 1 / 4,
         specialAbilities: [
           {
             name: "Nimble Escape",
@@ -49,13 +93,19 @@ Vue.createApp({
           {
             name: "Scimitar",
             description:
-              "Melee Weapon Attack: +4 to hit, reach 5 ft., one target. Hit: 5 (1d6 + 2) slashing damage.",
+              "Melee Weapon Attack: +4 to hit, reach 5 ft., one target.",
             attackBonus: 4,
-            damage: "1d6+2",
+            damage: "1d6 + 2 slashing damage",
+          },
+          {
+            name: "Shortbow",
+            description:
+              "Ranged Weapon Attack: +4 to hit, range 80/320 ft., one target.",
+            attackBonus: 4,
+            damage: "1d6 + 2 piercing damage",
           },
         ],
         legendaryActions: [],
-        homebrew: [],
       },
     };
   },
@@ -87,14 +137,20 @@ Vue.createApp({
         if (obj.hasOwnProperty(key)) {
           if (typeof obj[key] === "object" && obj[key] !== null) {
             let nested = this.getKeysAndValues(obj[key]);
-            keysAndValues.push(key);
+            if (!(key == 0)) {
+              keysAndValues.push(key + ":");
+            }
             // Push each key-value pair from nested object\
             nested.forEach((entry) => {
               keysAndValues.push(`${entry}`);
             });
           } else {
             // Push key-value pair for primitive values
-            keysAndValues.push(`${key}:${obj[key]}`);
+            if (isNaN(parseInt(key))) {
+              keysAndValues.push(`${key}: ${obj[key]}`);
+            } else {
+              keysAndValues.push(`${obj[key]}`);
+            }
           }
         }
       }
@@ -146,7 +202,13 @@ Vue.createApp({
   },
   computed: {
     getCreatureList: function () {
-      console.log(this.getKeysAndValues(this.currentCreature));
+      description = [];
+      let list = this.getKeysAndValues(this.currentCreature);
+      for (let key in list) {
+        splitted = list[key].split(":");
+        description[splitted[1]] = splitted[0];
+      }
+      return list;
     },
   },
 }).mount("#app");
